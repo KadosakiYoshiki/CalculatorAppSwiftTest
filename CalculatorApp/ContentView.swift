041ca7b106d34ce5a85134f97cdf7cc8dfdd37bf
門崎　由暉 <kadosakiyoshiki@kanzakiyuakiranoMac.local>
2020-04-22 23:51:49 -0700

    initial commit

---
+++ CalculatorApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorApp
+//
+//  Created by 門崎　由暉 on 2020/04/22.
+//  Copyright © 2020 門崎　由暉. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
